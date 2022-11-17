690a9ba2658619ffa3d939a7d39823134c12d98b
Ivan Kotov <iamivankotov@gmail.com>
2022-11-17 03:04:23 +0000

    formal screens

---
+++ screens/StopwatchScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const StopwatchScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Stopwatch!</Text>
+    </View>
+  );
+};
+
+export default StopwatchScreen;
---
+++ screens/WorldClockScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const WorldClockScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>World!</Text>
+    </View>
+  );
+};
+
+export default WorldClockScreen;
